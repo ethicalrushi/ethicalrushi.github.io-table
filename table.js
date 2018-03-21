1e64adffb212cae3794928ef154cea2edd821dd2
Rushikesh <pupalerushikesh@gmail.com>
2018-03-21 10:45:40 +0530

    Initial commit

---
+++ table.js
@@ -0,0 +1,61 @@
+var response = [{
+					"name":"Rushikesh",
+					"branch":"CSE",
+					"college":"NIT Raipur"
+				    },
+				    {
+					"name":"Rakesh",
+					"branch":"CSE",
+					"college":"NIT Jalandhar"
+				    },
+				    {
+					"name":"Rahul",
+					"branch":"CSE",
+					"college":"IIT Bombay"
+				    },
+				    {
+					"name":"Saurabh",
+					"branch":"CSE",
+					"college":"VJTI"
+				    },
+				    {
+					"name":"Anish",
+					"branch":"Mechanical",
+					"college":"IIT Bombay"
+				    },
+				    {
+					"name":"Shashank",
+					"branch":"Metallurgy",
+					"college":"IIT Bombay"
+			    }];
+
+
+
+
+$(function() {
+	$.each(response, function(i, item) {
+		var $tr = $('<tr>').append(
+			$('<td>').text(item.name),
+			$('<td>').text(item.branch),
+			$('<td>').text(item.college)
+
+			).appendTo('#record');
+	});
+});
+
+
+$('.search').keyup(function(){
+	var searchterm = $(this).val().toLowerCase();
+	$('#record tbody tr').each(function(){
+		var lineStr = $(this).text().toLowerCase();
+		if(lineStr.indexOf(searchterm) === -1){
+			$(this).hide();
+		}
+		else{
+			$(this).show()
+
+		}
+		
+	});
+});
+
